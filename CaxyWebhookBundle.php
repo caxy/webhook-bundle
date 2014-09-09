f89c5d0db1a1fef3cbee665b6f6200075a342d70
Benjamin Doherty <bdoherty@caxy.com>
2014-09-09 12:28:49 -0500

    RegisterListenersPass available in older versions with different namespace.

--- CaxyWebhookBundle.php
+++ CaxyWebhookBundle.php
@@ -4,20 +4,29 @@ namespace Caxy\Bundle\WebhookBundle;
 
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\EventDispatcher\DependencyInjection\RegisterListenersPass;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Kernel;
 
 class CaxyWebhookBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
+        if (Kernel::MINOR_VERSION < 5) {
+            $pass = new \Symfony\Component\HttpKernel\DependencyInjection\RegisterListenersPass(
+              'webhook_event_dispatcher',
+              'webhook_event_listener',
+              'webhook_event_subscriber'
+            );
+        } else {
+            $pass = new \Symfony\Component\EventDispatcher\DependencyInjection\RegisterListenersPass(
+              'webhook_event_dispatcher',
+              'webhook_event_listener',
+              'webhook_event_subscriber'
+            );
+        }
         $container->addCompilerPass(
-            new RegisterListenersPass(
-                'webhook_event_dispatcher',
-                'webhook_event_listener',
-                'webhook_event_subscriber'
-            ),
-            PassConfig::TYPE_BEFORE_REMOVING
+          $pass,
+          PassConfig::TYPE_BEFORE_REMOVING
         );
     }
 }
